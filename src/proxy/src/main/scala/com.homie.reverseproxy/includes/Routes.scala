9e5daa90c3830237731c135184c40f43818f82cc
Max Olsson (Maxylan) <max100@live.se>
2024-02-04 14:23:54 +0100

    Route in Routes.scala now sets request to http scheme explicitly. Misc. platform/docker updates

--- src/proxy/src/main/scala/com.homie.reverseproxy/includes/Routes.scala
+++ src/proxy/src/main/scala/com.homie.reverseproxy/includes/Routes.scala
@@ -13,16 +13,30 @@ import scala.util.{Failure, Success}
 object Routes {
 	implicit val system: ActorSystem = ReverseProxy.system
 	implicit val materializer: ActorMaterializer = ReverseProxy.materializer
+	val apiHost = Properties.envOrNone("API_HOST"/*, "homie.api"*/)
+	val apiPort = Properties.envOrNone("API_PORT"/*, "10001"*/)
+	val homieHost = Properties.envOrNone("HOMIE_HOST"/*, "homie.httpd"*/)
+	val homiePort = Properties.envOrNone("HOMIE_PORT"/*, "10000"*/)
+
+	def requestHomie(targetUri: Uri, request: HttpRequest): Future[HttpResponse] = {
+		val targetRequest = HttpRequest(method = request.method, uri = targetUri, entity = request.entity)
+		val responseFuture: Future[HttpResponse] = Http().singleRequest(targetRequest)
+		return responseFuture
+	}
 
 	val route = {
+		require(apiHost != null, s"Failed to load \"API_HOST\" from environment.")
+		require(apiPort != null, s"Failed to load \"API_PORT\" from environment.")
+		require(homieHost != null, s"Failed to load \"HOMIE_HOST\" from environment.")
+		require(homiePort != null, s"Failed to load \"HOMIE_PORT\" from environment.")
+
 		extractRequest { request =>
 			pathPrefix("api") {
 				path(".*".r) { path =>
 					extractUri { uri =>
-						val targetUri = uri.withAuthority(Properties.envOrElse("API_HOST", "homie.api"), Properties.envOrElse("API_PORT", "10001").toInt)
-						val targetRequest = HttpRequest(method = request.method, uri = targetUri, entity = request.entity)
-						val responseFuture: Future[HttpResponse] = Http().singleRequest(targetRequest)
-						onComplete(responseFuture) {
+						// Build targetUri to the API (homie.api)
+						val targetUri = uri.withAuthority(apiHost.get, apiPort.get.toInt).withScheme("http")
+						onComplete(requestHomie(targetUri, request)) {
 							case Success(response) => complete(response)
 							case Failure(ex) => complete(s"Request failed: ${ex.getMessage}")
 						}
@@ -32,10 +46,9 @@ object Routes {
 				// Default route for other requests
 				path(".*".r) { path =>
 					extractUri { uri =>
-						val targetUri = uri.withAuthority(Properties.envOrElse("HOMIE_HOST", "homie.httpd"), Properties.envOrElse("HOMIE_PORT", "10000").toInt)
-						val targetRequest = HttpRequest(method = request.method, uri = targetUri, entity = request.entity)
-						val responseFuture: Future[HttpResponse] = Http().singleRequest(targetRequest)
-						onComplete(responseFuture) {
+						// Build targetUri to "homie" (homie.httpd)
+						val targetUri = uri.withAuthority(homieHost.get, homiePort.get.toInt).withScheme("http")
+						onComplete(requestHomie(targetUri, request)) {
 							case Success(response) => {
 								complete(response)
 							}
