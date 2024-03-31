63d4a170d8ca0726b77931b18c69149d35bbdf2a
Max Olsson (Maxylan) <max100@live.se>
2024-03-31 16:50:01 +0200

    Moved controller's formatting to toplevel

--- src/api/Versions/v1/Controllers/PlatformsController.cs
+++ src/api/Versions/v1/Controllers/PlatformsController.cs
@@ -1,3 +1,5 @@
+namespace Homie.Api.v1.Controllers;
+
 using Asp.Versioning;
 using Homie.Api.v1.Handlers;
 using Homie.Api.v1.TransferModels;
@@ -5,92 +7,89 @@ using Homie.Utilities.Attributes;
 using Microsoft.AspNetCore.Mvc;
 using Homie.Database.Models;
 
-namespace Homie.Api.v1.Controllers
+[ApiVersion(Version.Name, Deprecated = Version.Deprecated)]
+[Route("{v:apiVersion}/platforms")]
+[Produces("application/json")]
+[ApiController]
+public class PlatformsController : ControllerBase
 {
-    [ApiVersion(Version.Name, Deprecated = Version.Deprecated)]
-    [Route("{v:apiVersion}/platforms")]
-    [Produces("application/json")]
-    [ApiController]
-    public class PlatformsController : ControllerBase
+    protected PlatformsHandler handler { get; init; }
+    protected UsersHandler usersHandler { get; init; }
+    public PlatformsController(PlatformsHandler _platformsHandler, UsersHandler _usersHandler) {
+        handler = _platformsHandler;
+        usersHandler = _usersHandler;
+    }
+
+    /// <summary>
+    /// (Development) "GET" All platforms registered in the database.
+    /// </summary>
+    /// <returns><see cref="PlatformDTO"/>[]</returns>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     GET /platforms
+    /// </remarks>
+    /// <response code="200">Returns an array of Platforms</response>
+    /// <response code="423">If used in a production build (Locked)</response>
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status423Locked)]
+    [EnvironmentDependant(ApiEnvironments.Development)]
+    [HttpGet]
+    public async Task<ActionResult<PlatformDTO[]>> GetAllPlatforms()
     {
-        protected PlatformsHandler handler { get; init; }
-        protected UsersHandler usersHandler { get; init; }
-        public PlatformsController(PlatformsHandler _platformsHandler, UsersHandler _usersHandler) {
-            handler = _platformsHandler;
-            usersHandler = _usersHandler;
+        var result = await handler.GetAllAsync();
+        return result;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns><see cref="CreatePlatformSuccess"/></returns>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     POST /platforms/create
+    ///     {
+    ///        "name": "Test Platform",
+    ///        "master_pswd": "password123"
+    ///     }
+    ///
+    /// </remarks>
+    /// <response code="201">Returns the newly created item</response>
+    /// <response code="400">If the item is null</response>
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [HttpPost("create")]
+    public async Task<ActionResult<CreatePlatformSuccess>> CreatePlatform(CreatePlatform newPlatform)
+    {
+        var createPlatformResult = await handler.PostAsync((PlatformDTO) newPlatform, newPlatform.Username);
+        if (createPlatformResult.Value is null) {
+            return createPlatformResult.Result!;
         }
 
-        /// <summary>
-        /// (Development) "GET" All platforms registered in the database.
-        /// </summary>
-        /// <returns><see cref="PlatformDTO"/>[]</returns>
-        /// <remarks>
-        /// Sample request:
-        ///
-        ///     GET /platforms
-        /// </remarks>
-        /// <response code="200">Returns an array of Platforms</response>
-        /// <response code="423">If used in a production build (Locked)</response>
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status423Locked)]
-        [EnvironmentDependant(ApiEnvironments.Development)]
-        [HttpGet]
-        public async Task<ActionResult<PlatformDTO[]>> GetAllPlatforms()
-        {
-            var result = await handler.GetAllAsync();
-            return result;
+        CreateUser newUser = new CreateUser {
+            PlatformId = (uint) createPlatformResult.Value.Id!,
+            Username = newPlatform.Name,
+            Group = UserGroup.Guest
+        };
+
+        var createUserResult = await usersHandler.PostAsync((UserDTO) newUser);
+        if (createUserResult.Value is null) {
+            return createUserResult.Result!;
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="item"></param>
-        /// <returns><see cref="CreatePlatformSuccess"/></returns>
-        /// <remarks>
-        /// Sample request:
-        ///
-        ///     POST /platforms/create
-        ///     {
-        ///        "name": "Test Platform",
-        ///        "master_pswd": "password123"
-        ///     }
-        ///
-        /// </remarks>
-        /// <response code="201">Returns the newly created item</response>
-        /// <response code="400">If the item is null</response>
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [HttpPost("create")]
-        public async Task<ActionResult<CreatePlatformSuccess>> CreatePlatform(CreatePlatform newPlatform)
-        {
-            var createPlatformResult = await handler.PostAsync((PlatformDTO) newPlatform, newPlatform.Username);
-            if (createPlatformResult.Value is null) {
-                return createPlatformResult.Result!;
-            }
-
-            CreateUser newUser = new CreateUser {
-                PlatformId = (uint) createPlatformResult.Value.Id!,
-                Username = newPlatform.Name,
-                Group = UserGroup.Guest
-            };
-
-            var createUserResult = await usersHandler.PostAsync((UserDTO) newUser);
-            if (createUserResult.Value is null) {
-                return createUserResult.Result!;
-            }
-
-            return CreatedAtAction(
-                nameof(GetAllPlatforms), 
-                new { 
-                    platform_id = createPlatformResult.Value!.Id,
-                    user_id = createUserResult.Value!.Id
-                }, 
-                new CreatePlatformSuccess(
-                    createPlatformResult.Value,
-                    createUserResult.Value
-                )
-            );
-        }
+        return CreatedAtAction(
+            nameof(GetAllPlatforms), 
+            new { 
+                platform_id = createPlatformResult.Value!.Id,
+                user_id = createUserResult.Value!.Id
+            }, 
+            new CreatePlatformSuccess(
+                createPlatformResult.Value,
+                createUserResult.Value
+            )
+        );
     }
 }
 
--- src/api/Versions/v1/Controllers/UsersController.cs
+++ src/api/Versions/v1/Controllers/UsersController.cs
@@ -1,3 +1,5 @@
+namespace Homie.Api.v1.Controllers;
+
 using Asp.Versioning;
 using Homie.Api.v1.Handlers;
 using Homie.Api.v1.TransferModels;
@@ -5,42 +7,39 @@ using Homie.Utilities.Attributes;
 using Microsoft.AspNetCore.Mvc;
 using Homie.Database.Models;
 
-namespace Homie.Api.v1.Controllers
+[ApiVersion(Version.Name, Deprecated = Version.Deprecated)]
+[Route("{v:apiVersion}/users")]
+[Produces("application/json")]
+[ApiController]
+public class UsersController : ControllerBase
 {
-    [ApiVersion(Version.Name, Deprecated = Version.Deprecated)]
-    [Route("{v:apiVersion}/users")]
-    [Produces("application/json")]
-    [ApiController]
-    public class UsersController : ControllerBase
+    protected UsersHandler handler { get; init; }
+    protected PlatformsHandler platformsHandler { get; init; }
+    public UsersController(UsersHandler _usersHandler, PlatformsHandler _platformsHandler) 
     {
-        protected UsersHandler handler { get; init; }
-        protected PlatformsHandler platformsHandler { get; init; }
-        public UsersController(UsersHandler _usersHandler, PlatformsHandler _platformsHandler) 
-        {
-            handler = _usersHandler;
-            platformsHandler = _platformsHandler;
-        }
+        handler = _usersHandler;
+        platformsHandler = _platformsHandler;
+    }
 
-        /// <summary>
-        /// (Development) "GET" All users registered in the database.
-        /// </summary>
-        /// <returns><see cref="UserDTO"/>[]</returns>
-        /// <remarks>
-        /// Sample request:
-        ///
-        ///     GET /users
-        /// </remarks>
-        /// <response code="200">Returns an array of Users</response>
-        /// <response code="423">If used in a production build (Locked)</response>
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status423Locked)]
-        [EnvironmentDependant(ApiEnvironments.Development)]
-        [HttpGet]
-        public async Task<ActionResult<UserDTO[]>> GetAllUsers()
-        {
-            var result = await handler.GetAllAsync();
-            return result;
-        }
+    /// <summary>
+    /// (Development) "GET" All users registered in the database.
+    /// </summary>
+    /// <returns><see cref="UserDTO"/>[]</returns>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     GET /users
+    /// </remarks>
+    /// <response code="200">Returns an array of Users</response>
+    /// <response code="423">If used in a production build (Locked)</response>
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status423Locked)]
+    [EnvironmentDependant(ApiEnvironments.Development)]
+    [HttpGet]
+    public async Task<ActionResult<UserDTO[]>> GetAllUsers()
+    {
+        var result = await handler.GetAllAsync();
+        return result;
     }
 }
 
